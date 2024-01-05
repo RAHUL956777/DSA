714820422dcb79b0412b18229323f4204976e943
RAHUL <dasr16583@gmail.com>
2024-01-05 14:38:05 +0530

    learning basic of c++

---
+++ demon.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    // std::cout<<"welcome Rahul"<<"\n";
+    // cout<<"welcome Rahul" << endl; //it is similar to \n
+    // cout<<"welcome Rahul";
+
+    // int x,y;
+    // cin>>x>>y;
+    // cout<<"The value of x is : "<<x << " and the value of y : "<<y;
+
+
+    // int
+    int dataType = 10;
+    // long - it's gives you to store more wider range for integer
+    long x = 1250;
+    // long long - it's gives you to store more wider range compare to long for integer 
+
+
+    
+
+    return 0;
+}
