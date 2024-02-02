399008a610b3098c677eadf83d7c5ecef798f2fb
RAHUL <dasr16583@gmail.com>
2024-02-02 20:42:06 +0530

    learning some new type of pattern

--- patterns.cpp
+++ patterns.cpp
@@ -126,7 +126,7 @@ void print10(int n){
     for(int i=0;i<n;i++){
         if(i % 2 ==0) start = 1;
         else start = 0;
-        for(int j = 0;j<i;j++){
+        for(int j = 0;j<=i;j++){
             cout<<start;
             start = 1 - start;
         }
@@ -134,11 +134,59 @@ void print10(int n){
     }
 }
 
+void print11(int n){
+    int space = 2*(n-1);
+    for(int i =1;i<=n;i++){
+        //number
+            for(int j=1;j<=i;j++){
+                cout<<j;
+            }
+        //space
+            for(int j=1;j<=space;j++){
+                cout<<" ";
+            }
+        //number
+        for(int j=i;j>=1;j--){
+                cout<<j;
+            }
+            cout<<endl;
+            space -= 2;
+    }
+}
+
+void print12(int n){
+    int num = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num += 1;
+        }
+        cout<<endl;
+    }
+}
+
+void print13(int n){
+    for(int i=0;i<n;i++){
+        for(char ch='A';ch<='A'+ i; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+vois print14(int n){
+    for(int i=0;i<n;i++){
+        for(char ch = 'A';ch<='A'+(n-i+1);ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
         int n;
         cin>>n;
-        print9(n);
+        print13(n);
         
     return 0;
 }
