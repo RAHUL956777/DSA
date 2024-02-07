3db01e2a40173ad3689c4133536161be595c6cf7
RAHUL <dasr16583@gmail.com>
2024-02-07 21:21:40 +0530

    working on some new type of pattern

--- patterns.cpp
+++ patterns.cpp
@@ -227,20 +227,66 @@ void print17(int n){
     }
 }
 
-// void print18(int n){
-//     for(int i=0;i<n;i++){
-//         //star
-//             for(int i)
-//         //space
+void print18(int n){
+    int iniS = 0;
+    for(int i=0;i<n;i++){
+        //star
+            for(int j=1;j<=n-i;j++){
+                cout<<"*";
+            }
+        //space
+                for(int j=0;j<iniS;j++){
+                    cout<<" ";
+                }
+        //star
+        for(int j=1;j<=n-i;j++){
+                cout<<"*";
+            }
+            iniS += 2;
+            cout<<endl;
+    }
 
-//         //star
-//     }
-// }
+    iniS = 2*n-2;
+    for(int i =1;i<=n;i++){
+        //star
+        for(int j = 1;j<=i;j++){
+            cout<<"*";
+        }
+        //space
+            for(int j=0;j<iniS;j++){
+                cout<< " ";
+            }
+        //star
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        iniS -= 2;
+        cout<<endl;
+
+    }
+}
+
+void print19(int n){
+    for(int i =1;i<=2*n-1;i++){
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+        //stars
+            for(int j =1;j<=stars;j++ ){
+                cout<<"*";
+            }
+        //spaces
+
+        //stars
+        for(int j =1;j<=stars;j++ ){
+                cout<<"*";
+            }
+    }
+}
 
 int main(){
         int n;
         cin>>n;
-        print17(n);
+        print18(n);
         
     return 0;
 }
