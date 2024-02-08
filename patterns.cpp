f76c490bbbe37e321c25d285602529e6cd4d3c52
RAHUL <dasr16583@gmail.com>
2024-02-08 12:48:58 +0530

    learning some new pattern

--- patterns.cpp
+++ patterns.cpp
@@ -267,26 +267,47 @@ void print18(int n){
 }
 
 void print19(int n){
+    int spaces = 2*n-2;
     for(int i =1;i<=2*n-1;i++){
         int stars = i;
         if(i>n) stars = 2*n-i;
         //stars
-            for(int j =1;j<=stars;j++ ){
+            for(int j=1;j<=stars;j++ ){
                 cout<<"*";
             }
         //spaces
-
+                for(int j=1;j<=spaces;j++){
+                    cout<<" ";
+                }
         //stars
-        for(int j =1;j<=stars;j++ ){
+        for(int j=1;j<=stars;j++ ){
                 cout<<"*";
             }
+
+            cout<<endl;
+            if(i<n) spaces -= 2;
+            else spaces += 2;   
+    }
+}
+
+void print20(int n){
+    for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+         if(i==0 ||i==n-1 || j==0 || j==n-1) cout<<"*";
+        else cout<<" ";
+       }
+         cout<<endl;
     }
 }
 
 int main(){
+//     #ifndef ONLINE_JUDGE
+// freopen(“input.txt”, “r”, stdin);
+// freopen(“output.txt”, “w”, stdout);
+// #endif
         int n;
         cin>>n;
-        print18(n);
+        print20(n);
         
     return 0;
 }
