13a5f5dab421059229ff33edc8854200f3ac6ccd
RAHUL <dasr16583@gmail.com>
2024-02-17 11:24:51 +0530

    learning all types of data structure and how to implement to making of caontainer

--- stl/stl.cpp
+++ stl/stl.cpp
@@ -39,7 +39,182 @@ void explainVector(){
         vector<int>v1(5,20);
         vector<int>v2(v1);//{20,20,20,20,20} //it creates a copy of v1
 
+        //access data from vector
+        vector<int>::iterator it = v.begin();//it points to the first element of the vector
+        it++;
+        cout<<*(it)<<" "; 
+
+        //more iterators
+        vector<int>::iterator it = v.end();//it points to the last element of the vector
+
+        vector<int>::iterator it = v.rend();//it points to the last element of the vector
+
+        cout<<v[0]<<" "<<v.at(0)<<endl;//both are same
+
+        cour<<v.back()<<" "<<v.front()<<endl;//it returns the last and first element of the vector
+
+        //print the vector
+
+        for(vetor<int>::iterator it = v.begin(); it != v.end();it++){
+            cout<<*(it)<<" ";
+        }
+
+//here auto use used for automatically access the dynamic data types
+        for(auto it = v.begin()l it != v.end(); it++){
+            cout<<*(it) <<" ";
+        }
+
+        for(auto it : v){
+            cout<<it<<" ";
+        }
+
+        //deletion of vector
+        v.erase(v.begin()+1); //{10, 20, 30, 40} it will delete the 20
+
+        //{10,20,30,40,50} after deletion it will remains {10,20,50}
+        v.erase(v.begin()+2, v.begin()+4); //[start,end] end is not included
+
+
+        //insert
+        vetor<int>v(2,100); //{100,100}
+        v.insert(v.begin(),300); //{300,100,100}
+        //if i insert 2  no fo 10 {300,10,10,100,100} 
+        v.insert(v.begin()+1,2,10)//[starting position, howmany data will to insert, the data will to insert]
+
+        vector<int> copy(2,50);//{50,50}
+        v.insert(v.begin(),copy.begin(),copy.end());//{50,50,300,10,10,100,100} [starting,after end]
+
+        cout<<v.size()//it will return the length of the vector
+
+        v.pop_back();//it will popout the last element of the vector
+
+        //v1 => {10,20}
+        //v2 => {30,40}
+        v1.swap(v2); //v1=>{30,40} v2=>{10,20}
+
+        v.clear();//erase the entire vector
+
+        cout<<v.empty();//it will help to check the vetor is empty or not
 }
+
+//*insert in a vector is a vwry costly opration , vwtor works like singly linked list
+
+
+//*for list doubly linkedlist are maintained
+void explainList(){
+    list<int>ls;
+
+    ls.push_back(2);//{2}
+    ls.emplace_back(4);//{2,4}
+
+    ls.push_front(5); // {5,2,4}
+    //rest functions same as vector
+    //begin,end,rbegin,rend,clear,insert,size,swap
+}
+
+void explainDeque(){
+    deque<int>dq;
+        dq.push_back(1);//{1}
+        dq.emplace_back(2);//{1,2}
+        dq.push_front(4);//{4,1,2}
+        dq.emplace_front(3);//{3,4,1,2}
+
+
+        dq.pop_back();//{3,4,1}
+        dq.pop_front();//{4,1}
+
+        dq.back();
+        dq.front();
+
+        //rest function same as vector
+        //begin,end,ebegin,rend,clear,insert,size,swap
+}
+
+void explainStack(){
+    //stack follows LIFO operations
+    stack<int>st;
+    st.push(1);//{1}
+    st.push(2);//{2,1}
+    st.push(3);//{3,2,1}
+    st.push(4);//{4,3,2,1}
+    st.emplace(5);//{5,4,3,2,1}
+
+    cout<<st.top(); //parints 5 "** st[2] is invalid **"
+
+    st.pop();//{4,3,2,1}
+
+    cout<<st.top();//3
+
+    cout<<st.size();//4
+
+    cout<<st.empty();//false
+
+    //swap
+    stack<int>st1,st2;
+    st1.swap(st2);
+
+}
+
+void explainQuesu(){
+    //it follows FIFO
+    queue<int>q;
+
+    q.push(1);//{1}
+    q.push(2);//{1,2}
+    q.aplice(4);//{1,2,4}
+
+    q.back() += 5
+
+    cout<<q.back(); // prints 9
+
+    cout<<q.front();//prints 1
+
+    q.pop();// {2,9}
+
+    cout<<q.front(); // prints 2
+
+    //size swap empty same as stack
+
+}
+
+//*in priority queue tree data structure are maintained it not stored data like linear manner
+void explainPQ(){
+    priority_queue<int>pq;
+
+//max heap 
+    pq.push(5);//{5}
+    pq.push(2);//{5,2}
+    pq.push(8);//{8,5,2}
+    pq.emiplice(10); // {10,8,5,2}
+
+    cout<<pq.top(); //prints 10
+
+    pq.pop();// {8,5,2}
+
+    cout<<pq.top(); //print 8
+
+    //size swap empty function same as others
+
+ //*note == push and pop operations time complexicity is lon n abd top = O(1)
+
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(5); //{5}
+    pq.push(2);//{2,5}
+    pq.push(8);//{2,5,8}
+    pq.emplice(10); //{2,5,8,10}
+
+    cout << pq.top(); //prints 2
+}
+
+// * set always unique and stored in sorted order
+void explainSet(){
+    set<int>set;
+    set.insert(1); //{1}
+    set.emplice(2);//{1,2}
+    set.emplice(2);//{1,2} //set should be unique thats why it 
+}
+
 
 int main(){
 
