ea139c8c771684eec3a54e467baba59c7e705cd4
RAHUL <dasr16583@gmail.com>
2024-03-06 21:11:42 +0530

    learning some new math tricS

--- math.cpp
+++ math.cpp
@@ -51,20 +51,36 @@ void armstrong(int n){
 
 
 //find all divisors
-void divisors(int num){
-    for(int i=1;i<=num;i++){
-        if(num%i == 0){
+void printdivisors(int n){
+    for(int i=1;i<=n;i++){
+        if(n%i == 0){
             cout<<i<<" ";
         }
     }
 }
 
+//another way to find factors
+void printdivisorsanother(int n){
+    vector<int> ls;
+
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i == 0){
+            ls.push_back(i);
+            if((n/i) != i){
+                ls.push_back(n/i);
+            }
+        }
+    }
+    sort(ls.begin(), ls.end());
+    for(auto it:ls) cout<<it<< " ";
+}
+
 
 int main(){
-    int num;
-    cout<<"enter number";
-    cin>>num;
-    divisors(num);
+    int n;
+    cout<<"enter number : ";
+    cin>>n;
+    printdivisorsanother(n);
 
     return 0;
 }
